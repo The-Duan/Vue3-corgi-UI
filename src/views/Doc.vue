7985cf1048f2798acba0adef480edc80276086da
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-04-22 06:17:51 +0800

    初始化aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,9 +1,42 @@
 <template>
-  <div>
+  <div class="layout">
     <Topnav/>
     <div class="content">
-      <aside>边栏</aside>
-      <main>主内容</main>
+      <aside>
+        <div>
+          <h2>文档</h2>
+          <ol>
+            <li>
+              <router-link to="/doc/introduce">介绍</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/install">安装</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/get-started">快速上手</router-link>
+            </li>
+
+          </ol>
+          <h2>组件列表</h2>
+          <ol>
+            <li>
+              <router-link to="/doc/switch">Switch 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/button">Button 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/dialog">Dialog 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/tabs">Tabs 组件</router-link>
+            </li>
+          </ol>
+        </div>
+      </aside>
+      <main>
+        <router-view />
+      </main>
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,45 @@
 <template>
-<div>
-  <Topnav/>
-  <div class="banner">
-    <h1>轱辘UI</h1>
-    <h2>一款 普普通通, 平平无奇 的 UI 框架</h2>
-    <p class="actions">
-      <a href="">GitHub</a>
-      <a href="">开始</a>
-    </p>
+  <div>
+    <Topnav/>
+    <div class="banner">
+      <h1>轱辘UI</h1>
+      <h2>一款 普普通通, 平平无奇 的 UI 框架</h2>
+      <p class="actions">
+        <a href="https://github.com/simple-ice/crisps-ui">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
+    </div>
   </div>
-</div>
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from '../components/Topnav.vue';
 
 export default {
-  components:{Topnav}
-}
+  components: {Topnav}
+};
 </script>
 
 <style lang="scss" scoped>
-
+.banner{
+  text-align: center;
+  padding: 50px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  >h2 {
+    margin: 20px 0;
+  }
+  >.actions{
+    >a{
+      margin: 0 8px;
+      background: rgb(249,191,175);
+      display: inline-block;
+      color: rgb(206,109,70);
+      padding: 8px 24px;
+      border-radius: 8px;
+    }
+  }
+}
 </style>
