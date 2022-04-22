c9506fcdf9fe1fdf3c4630eba2f25abe3324112c
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-04-22 20:33:01 +0800

    路由切换逻辑

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  switch文档
+</div>
+</template>
+
+<script lang="ts">
+// import Vue from 'vue';
+// import {Component} from 'vue-property-decorator';
+//
+// @Component
+// export default class App extends Vue {
+//
+// }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
