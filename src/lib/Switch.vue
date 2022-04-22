8b1e3c5a7dcfb323447c2aff24bc6a292fb8408f
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-04-23 07:46:10 +0800

    switch按钮可以切换äº

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,19 +1,20 @@
 <template>
-  <button class="crisps-switch" @click="toggle" :class="{checked}">
+  <button class="crisps-switch" @click="toggle" :class="{checked:value}">
     <span></span>
   </button>
 </template>
 
 <script lang="ts">
-import {ref} from 'vue';
 
 export default {
-  setup() {
-    const checked = ref(false);
+  props: {
+    value: Boolean
+  },
+  setup(props,context) {
     const toggle = () => {
-      checked.value = !checked.value;
+      context.emit('input',!props.value)
     };
-    return {checked, toggle};
+    return {toggle};
   }
 };
 </script>
@@ -29,6 +30,7 @@ $h2: $h - 4px;
   border-radius: $h/2;
   position: relative;
 }
+
 span {
   position: absolute;
   top: 2px;
@@ -37,13 +39,19 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
+
 .crisps-switch.checked {
   background: blue;
 }
 
 .crisps-switch.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+
+.crisps-switch:focus {
+  outline: none
 }
 
 //.crisps-switch {
