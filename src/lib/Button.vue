4dd635d4f3234189464fbb2ff4724a37f0d1a728
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-04-23 18:35:04 +0800

    创建Button按钮

---
+++ src/lib/Button.vue
@@ -0,0 +1,17 @@
+<template>
+$END$
+</template>
+
+<script lang="ts">
+ import Vue from 'vue';
+ import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class App extends Vue {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
