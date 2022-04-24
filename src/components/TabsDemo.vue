0bd0a3168262db135edc5166cfe1e8582a14c088
旘鸢 <10454967+DuanYouNing@user.noreply.gitee.com>
2022-04-24 21:01:26 +0800

    实现下划线

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,19 +1,27 @@
 <template>
-<div>
-  <h1>Tabs 标签页</h1>
-  <Tabs>
-    <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2">内容2</Tab>
-  </Tabs>
-</div>
+  <div>
+    <h1>Tabs 标签页</h1>
+    <Tabs v-model:selected="x">
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2">内容2</Tab>
+    </Tabs>
+  </div>
 </template>
 
 <script lang="ts">
-import Tabs from '../lib/Tabs.vue'
-import Tab from '../lib/Tab.vue'
+import Tabs from '../lib/Tabs.vue';
+import Tab from '../lib/Tab.vue';
+import {ref} from 'vue';
+
 export default {
-  components:{Tab,Tabs}
-}
+  components: {Tab, Tabs},
+  setup() {
+    const x = ref('导航1')
+    return {
+      x
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
